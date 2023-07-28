8a3960c21ba7070af0c501c7b9bbb34d86420d41
AlexLeo <dudealexlion@gmail.com>
2023-07-28 14:34:33 -0400

    BaseTest implementation

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,4 +1,24 @@
 package tests.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
 public class BaseTest {
+
+    private WebDriver driver;
+    @BeforeTest
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+        driver=null;
+    }
 }
