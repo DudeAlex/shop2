75b03cab4a2ada4f59e8c9c4c1ad9d1c08cced66
AlexLeo <dudealexlion@gmail.com>
2023-08-06 21:41:44 -0400

    done many things

--- src/test/java/pom/base/BaseModelPage.java
+++ src/test/java/pom/base/BaseModelPage.java
@@ -6,8 +6,15 @@ import org.openqa.selenium.support.PageFactory;
 
 public abstract class BaseModelPage extends BaseModel {
 
+    private WebDriver driver;
     public BaseModelPage(WebDriver driver){
 
+        this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    @Override
+    public WebDriver getDriver() {
+        return driver;
     }
 }
